3202a2d8f93358e0d02cd228f82f2eb3a50b16e3
albrosmar2 <albrosmar2@localhost>
2026-04-01 12:07:26 +0200

    Merge branch 'main' of https://github.com/matpassan/prueba into main

---
+++ github/src/prueba/Albert.java
@@ -0,0 +1,16 @@
+package prueba;
+
+public class Albert {
+	
+	private static void Saludador(String nombre) {
+		System.out.println("Hola soy "+nombre);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Albert";
+		
+		Saludador(name);
+	}
+
+}
