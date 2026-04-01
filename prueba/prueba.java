6bad35c254eec1352eab3132358fcbb77996580b
matpassan <matpassan@alu.edu.gva.es>
2026-04-01 09:58:22 +0200

    Update prueba.java

--- prueba/prueba.java
+++ prueba/prueba.java
@@ -5,7 +5,7 @@ public class prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hola soy mateo");
+		System.out.println("hola soy gabriel");
 	}
 
 }
